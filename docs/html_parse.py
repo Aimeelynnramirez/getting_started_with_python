38f4713e1fd2adef48aa0e26d40912a9f17a3c10
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-09-22 00:10:03 -0400

    adding these docs to this empty spaces

---
+++ docs/html_parse.py
@@ -0,0 +1,67 @@
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+
+print("this is a title:", soup.title)
+print("this is the name of what we are calling tags", soup.title.name)
+print("this is a title's name:", soup.title.string)
+print("this is a title's parent's name:", soup.title.parent.name)
+print("this is getting the p tag:", soup.p)
+print("this is getting the p class name:", soup.p['class'])
+print("this is getting all links",soup.find_all('a'))
+#this is getting all the links readable on the page
+for link in soup.find_all('a'):
+    print(link.get('href'))
+#this is getting html_doc
+
+print(soup.prettify())
+#this is getting all the texts on the page
+print(soup.get_text())
+
+
+# <html>
+#  <head>
+#   <title>
+#    The Dormouse's story
+#   </title>
+#  </head>
+#  <body>
+#   <p class="title">
+#    <b>
+#     The Dormouse's story
+#    </b>
+#   </p>
+#   <p class="story">
+#    Once upon a time there were three little sisters; and their names were
+#    <a class="sister" href="http://example.com/elsie" id="link1">
+#     Elsie
+#    </a>
+#    ,
+#    <a class="sister" href="http://example.com/lacie" id="link2">
+#     Lacie
+#    </a>
+#    and
+#    <a class="sister" href="http://example.com/tillie" id="link2">
+#     Tillie
+#    </a>
+#    ; and they lived at the bottom of a well.
+#   </p>
+#   <p class="story">
+#    ...
+#   </p>
+#  </body>
+# </html>
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 Here is a fun little way to install your environment to best run it either in a program install or in your terminal. 
 Here is a fun few documentations to help get your python running: 
-
+Here are some simple ways to navigate that data structure:
 
 
 This is a great program to work with an IDE: 
