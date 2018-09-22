ac598b387e06bff506b549604b3df79c1c6cb57a
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-09-21 23:17:23 -0400

    simple variable running

---
+++ counter.py
@@ -0,0 +1,11 @@
+#the last file.py has variables, these can have names as well.
+#giving a value
+counter=500
+#gives a floating value
+miles=1000.0
+#giving the string value
+name="Aimee"
+
+print(counter)
+print(miles)
+print(name)
