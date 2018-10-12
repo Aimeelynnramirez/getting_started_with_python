baf8bac0c5cd6f0d09082488582faaa6a507e327
Aimee Lynn <aimeelynnramirez@gmail.com>
2018-10-12 19:49:12 -0400

    saying hello in python

--- comma.py
+++ comma.py
@@ -4,4 +4,5 @@ a,b,c = 10, 50, 100
 print(a)
 print(b)
 print(c)
+print("HEY there everybody this is how easy to print out", a)
 print("this prints all:",a,b,c)
---
+++ hello.py
@@ -0,0 +1,7 @@
+print("hello there everybody this is a new file for python and see it is simple as abc")
+a,b,c= 'kristine', 'mila' , 'gilbert'
+
+print("Name is:", a)
+print("Name b is:", b)
+print ("Name c is:",  c)
+
